6beee6166fac27da9815eaa48b441e2f2dfdbcb0
Deepanshu Yadav <yaadav.deepanshu@gmail.com>
2025-09-19 19:44:52 +0530

    safeEntry deployment terraform only

---
+++ provider.tf
@@ -0,0 +1,11 @@
+provider "aws" {
+  region = var.region
+}
+
+terraform {
+  backend "s3" {
+    bucket = "yaadav-tf-state"
+    key    = "terraform/state"
+    region = "us-east-1"
+  }
+}
---
+++ variables.tf
@@ -0,0 +1,7 @@
+variable "region" {
+  default = "us-east-1"
+}
+
+variable "app_name" {
+  default = "logo-server"
+}
